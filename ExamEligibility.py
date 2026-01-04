e2df2b936b0ca6be9d5abbf9a356a6b6388ea64d
Mohammed_Haider <mhfadoo@gmail.com>
2026-01-04 16:48:53 +0500

    After Class Projects

---
+++ ExamEligibility.py
@@ -0,0 +1,6 @@
+attendance = float(input("Enter your attendance percentage: "))
+
+if attendance >= 75:
+    print("You are eligible to appear in the exam.")
+else:
+    print("You are not eligible to appear in the exam.")
